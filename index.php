1b7ad733e0ae431fa894d44b72a5af09d93e94bd
Joseph Chua <RepeaterCreeper@users.noreply.github.com>
2021-11-24 22:23:03 -0700

    :zap: Added feature for removing clinic.

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
     require_once "init.php";
+
+    if (isset($_GET["delete"]) && isset($_GET["clinic"])) {
+        unset($DATA["Clinics"][$_GET["clinic"]]);
+        Utility::saveChanges();
+
+        header("location: /");
+    }
 ?>
 
 
@@ -24,11 +31,19 @@
             <h1 class="font-bold text-2xl">Clinic</h1>
             <a class="bg-indigo-600 px-8 py-2 font-medium text-white rounded-md hover:bg-indigo-800" href="new-clinic.php">Add Clinic</a>
         </div>
-        <div class="grid grid-cols-6 gap-4">
+        <div class="grid grid-cols-3 gap-4">
             <?php foreach ($DATA["Clinics"] as $clinicName => $patients) { ?>
                 <div class="border border-blue-500 shadow-md rounded-md text-center p-4">
                     <h1 class="font-bold text-2xl mb-4"><?= $clinicName; ?></h1>
-                    <a class="bg-indigo-700 py-2 px-4 font-bold text-white rounded-md hover:bg-indigo-800" href="/view-patients.php?clinic=<?= $clinicName ?>">View Patients</a>
+                    <div class="flex flex-col">
+                        <a class="bg-indigo-700 py-2 px-4 font-bold text-white rounded-md hover:bg-indigo-800" href="/view-patients.php?clinic=<?= $clinicName ?>">View Patient</a>
+                        <a class="bg-red-700 py-2 px-4 font-bold text-white rounded-md hover:bg-red-800 flex mt-4" href="?clinic=<?= $clinicName ?>&delete">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                            Delete Clinic
+                        </a>
+                    </div>
                 </div>
             <?php } ?>
         </div>
