5f0883e468fd0ee142570bd7f649fa7c78aa4993
Topher0120 <ctdinglasan0120@gmail.com>
2021-11-25 13:36:23 -0800

    Task 3 Completed Sorting Functionality

--- Utility.php
+++ Utility.php
@@ -13,4 +13,31 @@ class Utility {
         $data = file_get_contents("./data.txt");
         return unserialize($data);
     }
+
+    static function sortByName($patients, $asc = true) {
+        usort($patients, function($a,$b) {
+            return strcmp($a->getName(), $b->getName());
+        });
+
+        $out = $asc ? $patients : array_reverse($patients);
+        return $out;
+    }
+
+    static function sortByTime($patients, $asc = true) {
+        usort($patients, function($a, $b) {
+            return strcmp($a->getConsultationDetails("time"), $b->getConsultationDetails("time"));
+        });
+
+        $out = $asc ? $patients : array_reverse($patients);
+        return $out;
+    }
+
+    static function sortByConsultation($patients, $asc = true) {
+        usort($patients, function($a, $b) {
+            return strcmp($a->getConsultationDetails("done"), $b->getConsultationDetails("done"));
+        });
+        
+        $out = $asc ? $patients : array_reverse($patients);
+        return $out;
+    }
 }
